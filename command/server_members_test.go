019bf45dd0bfe2601df1706ea7bbdde0285d402f
prateek <prateekpandey14@gmail.com>
2017-02-13 13:49:03 +0530

    UT for server-members

--- command/server_members_test.go
+++ command/server_members_test.go
@@ -41,3 +41,25 @@ func TestServerMembersCommand_Run(t *testing.T) {
 		t.Fatalf("expected tags in output, got: %s", out)
 	}
 }
+
+func TestMembersCommand_Fails(t *testing.T) {
+	ui := new(cli.MockUi)
+	cmd := &ServerMembersCommand{Meta: Meta{Ui: ui}}
+
+	// Fails on misuse
+	if code := cmd.Run([]string{"some", "bad", "args"}); code != 1 {
+		t.Fatalf("expected exit code 1, got: %d", code)
+	}
+	if out := ui.ErrorWriter.String(); !strings.Contains(out, cmd.Help()) {
+		t.Fatalf("expected help output, got: %s", out)
+	}
+	ui.ErrorWriter.Reset()
+
+	// Fails on connection failure
+	if code := cmd.Run([]string{"-address=nope"}); code != 1 {
+		t.Fatalf("expected exit code 1, got: %d", code)
+	}
+	if out := ui.ErrorWriter.String(); !strings.Contains(out, "Error querying servers") {
+		t.Fatalf("expected failed query error, got: %s", out)
+	}
+}
